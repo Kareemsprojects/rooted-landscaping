aaf3334362327f60aa56379b5d99ebcbb549ad14
Kareemsprojects <kareem@mykreativekraft.com>
2026-06-23 18:48:47 -0400

    Delete rooted-landscapes-app/src directory

--- rooted-landscapes-app/src/components/MockupBanner.jsx
+++
@@ -1,7 +0,0 @@
-export default function MockupBanner() {
-  return (
-    <div className="mockup-banner">
-      MOCKUP PREVIEW — built by Rooted Landscapes' web team to show what a custom site could look like
-    </div>
-  )
-}
